de5d330f8cc49904baa3ec02c759b1e8f8af9575
Alessandro Battini <alessandro.battini@codiceplastico.com>
2025-06-06 17:42:54 +0200

    second protohackers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use serde::{Deserialize, Serialize};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
+#[derive(Deserialize, Debug)]
+struct Request {
+    method: String,
+    number: i32,
+}
+
+#[derive(Serialize)]
+struct Response {
+    method: String,
+    prime: bool,
+}
+
 #[tokio::main]
-async fn main()-> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:5001").await?;
 
     loop {
@@ -25,6 +38,24 @@ async fn handle_connection(mut stream: TcpStream) {
     loop {
         match stream.read(&mut buffer).await {
             Ok(size) if size != 0 => {
+                match serde_json::from_slice::<Request>(&buffer[..size]) {
+                    Ok(request) => {
+                        println!("{:?}",request);
+                        if request.method != "isPrime" {
+                            println!("richiesta malformata, {:?}", request);
+                            let _ = stream.write("malformed".as_bytes()).await;
+                            return;
+                        }
+                        let is_prime = check_if_prime(request.number).unwrap();
+                        let response = Response {method:"isPrime".to_string(), prime: is_prime};
+                        let _ = stream.write(serde_json::to_string(&response).unwrap().as_bytes()).await;
+                    }
+                    Err(_) => {
+                        let _ = stream.write("malformed".as_bytes()).await;
+                        return;
+                    }
+                };
+
                 if let Err(e) = stream.write_all(&buffer[..size]).await {
                     eprintln!("Error writing to socket: {}", e);
                 }
@@ -38,5 +69,29 @@ async fn handle_connection(mut stream: TcpStream) {
                 return;
             }
         }
+    }
+}
+
+fn check_if_prime(number: i32) -> Option<bool> {
+    if number < 2 {
+        return Some(false);
+    }
+    let has_divisor = (2..=number / 2).any(|x| number % x == 0);
+    Some(!has_divisor)
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = check_if_prime(4).unwrap();
+        assert_eq!(result, false);
+        let result = check_if_prime(13).unwrap();
+        assert_eq!(result, true);
+        let result = check_if_prime(144).unwrap();
+        assert_eq!(result, false);
+        let result = check_if_prime(7789).unwrap();
+        assert_eq!(result, true);
     }
 }
