de0a5cebeb93fb9ac25283b25a03204794600c15
Emanuele DelBono <emanuele@codiceplastico.com>
2025-06-09 16:49:23 +0200

    fixed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 #[derive(Deserialize, Debug)]
 struct Request {
     method: String,
-    number: i32,
+    number: f64,
 }
 
 #[derive(Serialize)]
@@ -30,35 +30,14 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer: [u8; 512] = [0; 512];
+fn handle_connection(stream: TcpStream) {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = stream.try_clone().unwrap();
     loop {
-        match stream.read(&mut buffer) {
+        let mut buffer: String = String::new();
+        match reader.read_line(&mut buffer) {
             Ok(size) if size != 0 => {
-                match serde_json::from_slice::<Request>(&buffer[..size]) {
-                    Ok(request) => {
-                        println!("{:?}", request);
-                        if request.method != "isPrime" {
-                            println!("richiesta malformata, {:?}", request);
-                            let _ = stream.write("malformed".as_bytes());
-                            return;
-                        }
-                        let is_prime = check_if_prime(request.number).unwrap();
-                        let response = Response {
-                            method: "isPrime".to_string(),
-                            prime: is_prime,
-                        };
-                        let _ = stream.write(serde_json::to_string(&response).unwrap().as_bytes());
-                    }
-                    Err(_) => {
-                        let _ = stream.write("malformed".as_bytes());
-                        return;
-                    }
-                };
-
-                if let Err(e) = stream.write_all(&buffer[..size]) {
-                    eprintln!("Error writing to socket: {}", e);
-                }
+                process_request(&mut writer, buffer);
             }
             Ok(_) => {
                 println!("Connection closed");
@@ -72,26 +51,68 @@ fn handle_connection(mut stream: TcpStream) {
     }
 }
 
-fn check_if_prime(number: i32) -> Option<bool> {
-    if number < 2 {
-        return Some(false);
-    }
-    let has_divisor = (2..=number / 2).any(|x| number % x == 0);
-    Some(!has_divisor)
+fn process_request(writer: &mut TcpStream, buffer: String) {
+    match serde_json::from_str::<Request>(&buffer.trim()) {
+        Ok(request) => {
+            println!("{:?}", request);
+            if request.method != "isPrime" {
+                send_malformed(writer);
+                return;
+            }
+            let is_prime = check_if_prime(request.number);
+            let response = Response {
+                method: "isPrime".to_string(),
+                prime: is_prime,
+            };
+
+            let _ = writer.write(serde_json::to_string(&response).unwrap().as_bytes());
+            let _ = writer.write(b"\n");
+            let _ = writer.flush();
+        }
+        Err(x) => {
+            println!("ERR: {:?}", x);
+            send_malformed(writer);
+            return;
+        }
+    };
 }
+
+fn send_malformed(writer: &mut TcpStream) {
+    let _ = writer.write_all(String::from("Malformed").as_bytes());
+    let _ = writer.write_all(b"\n");
+    let _ = writer.flush();
+}
+
+fn check_if_prime(num: f64) -> bool {
+    if num.fract() != 0.0 {
+        return false;
+    }
+    let num = num.trunc() as i64;
+    if num <= 1 {
+        return false;
+    }
+
+    for i in 2..=((num as f64).sqrt().floor() as i64) {
+        if num % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = check_if_prime(4).unwrap();
+        let result = check_if_prime(4.0);
         assert_eq!(result, false);
-        let result = check_if_prime(13).unwrap();
+        let result = check_if_prime(13.0);
         assert_eq!(result, true);
-        let result = check_if_prime(144).unwrap();
+        let result = check_if_prime(144.0);
         assert_eq!(result, false);
-        let result = check_if_prime(7789).unwrap();
+        let result = check_if_prime(7789.0);
         assert_eq!(result, true);
     }
 }
