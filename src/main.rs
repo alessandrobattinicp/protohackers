da318487ffab1fef455120b2c8e245e5f364b736
Emanuele DelBono <emanuele@codiceplastico.com>
2025-07-04 18:13:02 +0200

    first draft

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use serde::{Deserialize, Serialize};
-use std::io::{BufReader, BufWriter, Read, Write};
-use std::iter::Sum;
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-enum OperationType {
-    Unknown,
-    Insert(InsertStruct),
-    Query(QueryStruct),
+struct Clients {
+    username: String,      //lista di nomi
+    connections_list: i32, //lista di connessioni
 }
 
-struct InsertStruct {
-    timestamp: i32,
-    price: i32,
-}
+// use std::sync::mpsc;
+// use std::thread;
 
-struct QueryStruct {
-    mintime: i32,
-    maxtime: i32,
-}
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+
+//     thread::spawn(move || {
+//         let val = String::from("hi");
+//         tx.send(val).unwrap();
+//     });
+
+//     let received = rx.recv().unwrap();
+//     println!("Got: {received}");
+// }
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:5001").unwrap();
+    let lista = Vec::<String>::new();
 
     for stream in listener.incoming() {
         match stream {
             Ok(streamz) => {
-                thread::spawn(move || handle_connection(streamz));
+                thread::spawn(move || handle_connection(streamz, &lista));
             }
             Err(e) => {
                 println!("Tcp Error: {}", e);
@@ -35,30 +38,50 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(stream: TcpStream, clients_list: &Vec<String>) {
     println!("Un client si è connesso");
-    let mut packets: Vec<InsertStruct> = Vec::new();
 
     let connection_stream = stream.try_clone().unwrap();
     let mut reader = BufReader::new(&connection_stream);
     let mut writer = BufWriter::new(&connection_stream);
+
+    // 1 - welcome
+    let _ = writer.write_all("Welcome to budgetchat! What shall I call you?\n".as_bytes());
+    match writer.flush() {
+        Ok(_) => {}
+        Err(e) => {
+            println!("errore di flush {:?}", e);
+        }
+    }
+
+    // 2 - username
+    let mut username: String = String::new();
+    match reader.read_line(&mut username) {
+        Ok(size) if size != 0 => {
+            let is_valid = validate_username(username.as_bytes());
+            if !is_valid {
+                return;
+            }
+        }
+        Ok(_) => {
+            println!("ricevuto2 {:?}", username);
+        }
+        Err(e) => {
+            eprintln!("Read error: {}", e);
+        }
+    }
+
+    //3- avvisare gli altri client della connessione
+
     loop {
-        let mut buffer: [u8; 9] = [0; 9];
-        match reader.read_exact(&mut buffer) {
-            Ok(()) => match buffer.parse() {
-                OperationType::Insert(insert) => {
-                    packets.push(insert);
-                }
-                OperationType::Query(query) => {
-                    println!(
-                        "Query: mintime={}, maxtime={}",
-                        query.mintime, query.maxtime
-                    );
-                    let avg: i32 = average(&packets, query.mintime, query.maxtime);
-                    let _ = writer.write_all(&avg.to_be_bytes());
-                }
-                OperationType::Unknown => {}
-            },
+        let mut buffer: String = String::new();
+        match reader.read_line(&mut buffer) {
+            Ok(size) if size != 0 => {
+                println!("ricevuto {:?}", buffer);
+            }
+            Ok(_) => {
+                println!("ricevuto2 {:?}", buffer);
+            }
             Err(e) => {
                 eprintln!("Read error: {}", e);
                 break;
@@ -75,44 +98,28 @@ fn handle_connection(stream: TcpStream) {
     }
 }
 
-fn average(packets: &Vec<InsertStruct>, mintime: i32, maxtime: i32) -> i32 {
-    if mintime > maxtime {
-        return 0;
+fn validate_username(str: &[u8]) -> bool {
+    if str.len() < 1 {
+        return false;
     }
 
-    let valid_packets: Vec<&InsertStruct> = packets
-        .iter()
-        .filter(|is| is.timestamp >= mintime && is.timestamp <= maxtime)
-        .collect();
-
-    let count = valid_packets.len();
-    let total: i64 = valid_packets.iter().map(|is| is.price as i64).sum();
-
-    if count == 0 {
-        return 0;
-    }
-
-    (total / count as i64) as i32
-}
-
-trait ParseOperation {
-    fn parse(&self) -> OperationType;
-}
-
-impl ParseOperation for [u8; 9] {
-    fn parse(&self) -> OperationType {
-        match self[0] as char {
-            'I' => {
-                let timestamp = i32::from_be_bytes([self[1], self[2], self[3], self[4]]);
-                let price = i32::from_be_bytes([self[5], self[6], self[7], self[8]]);
-                OperationType::Insert(InsertStruct { timestamp, price })
-            }
-            'Q' => {
-                let mintime = i32::from_be_bytes([self[1], self[2], self[3], self[4]]);
-                let maxtime = i32::from_be_bytes([self[5], self[6], self[7], self[8]]);
-                OperationType::Query(QueryStruct { mintime, maxtime })
-            }
-            _ => OperationType::Unknown,
+    for c in str.to_owned() {
+        if !is_uppercase(c) || !is_lowercase(c) || !is_digit(c) {
+            return false;
         }
     }
+
+    true
+}
+
+fn is_uppercase(c: u8) -> bool {
+    c >= 65 && c <= 90
+}
+
+fn is_lowercase(c: u8) -> bool {
+    c >= 97 && c <= 122
+}
+
+fn is_digit(c: u8) -> bool {
+    c >= 48 && c <= 57
 }
