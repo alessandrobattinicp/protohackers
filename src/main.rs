e0e69a8c410081ab0a881422b2a6336ee54bc035
Emanuele DelBono <emanuele@codiceplastico.com>
2025-09-26 17:55:48 +0200

    ready to send messages

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-struct Clients {
-    username: String,      //lista di nomi
-    connections_list: i32, //lista di connessioni
+struct Client {
+    username: String,  //lista di nomi
+    stream: TcpStream, //lista di connessioni
 }
-
-// use std::sync::mpsc;
-// use std::thread;
-
-// fn main() {
-//     let (tx, rx) = mpsc::channel();
-
-//     thread::spawn(move || {
-//         let val = String::from("hi");
-//         tx.send(val).unwrap();
-//     });
-
-//     let received = rx.recv().unwrap();
-//     println!("Got: {received}");
-// }
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:5001").unwrap();
-    let lista = Vec::<String>::new();
+    let clients = Arc::new(Mutex::new(Vec::<Client>::new()));
 
     for stream in listener.incoming() {
+        let clients = Arc::clone(&clients);
         match stream {
             Ok(streamz) => {
-                thread::spawn(move || handle_connection(streamz, &lista));
+                thread::spawn(move || handle_connection(streamz, clients));
             }
             Err(e) => {
                 println!("Tcp Error: {}", e);
@@ -38,17 +25,20 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream, clients_list: &Vec<String>) {
+fn handle_connection(stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
     println!("Un client si è connesso");
 
     let connection_stream = stream.try_clone().unwrap();
+
     let mut reader = BufReader::new(&connection_stream);
     let mut writer = BufWriter::new(&connection_stream);
 
     // 1 - welcome
     let _ = writer.write_all("Welcome to budgetchat! What shall I call you?\n".as_bytes());
     match writer.flush() {
-        Ok(_) => {}
+        Ok(_) => {
+            println!("Nuova connessione ok");
+        }
         Err(e) => {
             println!("errore di flush {:?}", e);
         }
@@ -62,25 +52,85 @@ fn handle_connection(stream: TcpStream, clients_list: &Vec<String>) {
             if !is_valid {
                 return;
             }
+            println!("username: {:?}", username);
         }
-        Ok(_) => {
-            println!("ricevuto2 {:?}", username);
-        }
+        Ok(_) => {}
         Err(e) => {
             eprintln!("Read error: {}", e);
         }
     }
 
+    {
+        let mut list = clients.lock().unwrap();
+        let mut names = Vec::<String>::new();
+
+        for client in list.iter() {
+            println!("Sono nel for per il Join del secondo utente");
+            names.push(client.username.clone());
+            let connection_stream = client.stream.try_clone().unwrap();
+
+            let mut writer = BufWriter::new(&connection_stream);
+            let _ = writer.write_all(format!("* {} has joined the room\n", username).as_bytes());
+            match writer.flush() {
+                Ok(_) => {
+                    println!("Utente annunciato con:");
+                    println!("* {} has joined the room\n", username);
+                }
+                Err(e) => {
+                    println!("errore di flush {:?}", e);
+                }
+            }
+        }
+
+        let mut name_list = "* The room contains: ".to_string();
+        name_list.push_str(names.join(",").as_str());
+        name_list.push('\n');
+
+        println!("Inizio annuncio");
+        print!("{}", name_list);
+        println!("Fine annuncio");
+
+        let _ = writer.write_all(name_list.as_bytes());
+        match writer.flush() {
+            Ok(_) => {
+                println!("Invio lista utenti");
+            }
+            Err(e) => {
+                println!("errore di flush {:?}", e);
+            }
+        }
+
+        /*
+
+                          [Fri Sep 26 15:52:03 2025 UTC] [1client.test] NOTE:check starts
+                [Fri Sep 26 15:52:03 2025 UTC] [1client.test] NOTE:connected to 93.66.32.222 port 5001
+                [Fri Sep 26 15:52:06 2025 UTC] [1client.test] PASS
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:check starts
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:watchman connected to 93.66.32.222 port 5001
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:watchman joined the chat room
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:alice connected to 93.66.32.222 port 5001
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:bob connected to 93.66.32.222 port 5001
+                [Fri Sep 26 15:52:07 2025 UTC] [2clients.test] NOTE:alice joined the chat room
+        [Fri Sep 26 15:55:02 2025 UTC] [2clients.test] FAIL:message to 'watchman' was not correct (expected '[alice] I think I'm alone now'):  has joined the room
+
+                            */
+
+        username.truncate(username.len() - 1);
+        list.push(Client { username, stream });
+    }
+
     //3- avvisare gli altri client della connessione
+    // inviare il messaggio "username joins"
+    // inviare la lista dei presenti
 
     loop {
         let mut buffer: String = String::new();
         match reader.read_line(&mut buffer) {
             Ok(size) if size != 0 => {
-                println!("ricevuto {:?}", buffer);
+                println!("ricevuto riga 115 {:?}", buffer);
             }
             Ok(_) => {
-                println!("ricevuto2 {:?}", buffer);
+                // println!("ricevuto2 {:?}", buffer);
             }
             Err(e) => {
                 eprintln!("Read error: {}", e);
@@ -103,8 +153,8 @@ fn validate_username(str: &[u8]) -> bool {
         return false;
     }
 
-    for c in str.to_owned() {
-        if !is_uppercase(c) || !is_lowercase(c) || !is_digit(c) {
+    for c in str[..str.len() - 1].iter() {
+        if !is_uppercase(c) && !is_lowercase(c) && !is_digit(c) {
             return false;
         }
     }
@@ -112,14 +162,18 @@ fn validate_username(str: &[u8]) -> bool {
     true
 }
 
-fn is_uppercase(c: u8) -> bool {
-    c >= 65 && c <= 90
+fn is_backslashn(c: &u8) -> bool {
+    *c == 10
 }
 
-fn is_lowercase(c: u8) -> bool {
-    c >= 97 && c <= 122
+fn is_uppercase(c: &u8) -> bool {
+    *c >= 65 && *c <= 90
 }
 
-fn is_digit(c: u8) -> bool {
-    c >= 48 && c <= 57
+fn is_lowercase(c: &u8) -> bool {
+    *c >= 97 && *c <= 122
+}
+
+fn is_digit(c: &u8) -> bool {
+    *c >= 48 && *c <= 57
 }
