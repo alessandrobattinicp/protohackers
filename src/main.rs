63f64706eedc74395f51a71b4213d25798ebd329
Emanuele DelBono <emanuele@codiceplastico.com>
2025-10-17 18:17:10 +0200

    problem_5

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-struct Client {
-    username: String,  //lista di nomi
-    stream: TcpStream, //lista di connessioni
-}
+use regex::Regex;
+
+/*
+[Fri Oct 17 15:50:56 2025 UTC] [4notbogus.test] NOTE:PinkFrank450 joined the chat room
+[Fri Oct 17 15:50:58 2025 UTC] [4notbogus.test] FAIL:message to 'GreenEdward629' was not correct
+    (expected '[PinkFrank450] This is a product ID, not a Boguscoin: 7BCO8HH82v42s1r7NvGVkELN1r-OI50f7pcgUDwwjk2cCsTAE3rrI-1234'):
+    [PinkFrank450] This is a product ID, not a Boguscoin: 7YWHMfk9JZe0LM0g1ZauHuiSxhI-OI50f7pcgUDwwjk2cCsTAE3rrI-1234
+*/
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:5001").unwrap();
-    let clients = Arc::new(Mutex::new(Vec::<Client>::new()));
 
-    for stream in listener.incoming() {
-        let clients = Arc::clone(&clients);
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || handle_connection(stream, clients));
+    for client in listener.incoming() {
+        match client {
+            Ok(client) => {
+                handle_connection(client);
             }
             Err(e) => {
                 println!("Tcp Error: {}", e);
@@ -25,153 +27,89 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
-    println!("Un client si è connesso");
-
-    let connection_stream = stream.try_clone().unwrap();
-
-    let mut reader = BufReader::new(&connection_stream);
-    let mut writer = BufWriter::new(&connection_stream);
-
-    // 1 - welcome
-    let _ = writer.write_all("Welcome to budgetchat! What shall I call you?\n".as_bytes());
-    match writer.flush() {
-        Ok(_) => {
-            println!("Nuova connessione ok");
-        }
-        Err(e) => {
-            println!("errore di flush 43 {:?}", e);
-        }
-    }
-
-    // 2 - username
-    let mut username: String = String::new();
-    match reader.read_line(&mut username) {
-        Ok(size) if size >= 2 => {
-            let is_valid = validate_username(username.as_bytes());
-            if !is_valid {
-                return;
-            }
-            println!("username: {:?}", username);
-        }
-        Ok(_) => {
-            return;
-        }
-        Err(e) => {
-            eprintln!("Read error: {}", e);
-        }
-    }
-
-    {
-        let mut list = clients.lock().unwrap();
-        let mut names = Vec::<String>::new();
-
-        for client in list.iter() {
-            names.push(client.username.clone());
-            let connection_stream = client.stream.try_clone().unwrap();
-
-            let mut writer = BufWriter::new(&connection_stream);
-            let _ = writer.write_all(format!("* room joined by {}", username).as_bytes());
-            match writer.flush() {
-                Ok(_) => {
-                    println!("Utente annunciato con:");
-                    println!("* {} has joined the room\n", username);
-                }
-                Err(e) => {
-                    println!("{} errore di flush 81 {:?}", username, e);
-                }
-            }
-        }
-
-        let mut name_list = "* The room contains: ".to_string();
-        name_list.push_str(names.join(",").as_str());
-        name_list.push('\n');
-
-        let _ = writer.write_all(name_list.as_bytes());
-        match writer.flush() {
-            Ok(_) => {
-                println!("Invio lista utenti");
-            }
-            Err(e) => {
-                println!("errore di flush 96 {:?}", e);
-            }
-        }
-
-        username.truncate(username.len() - 1);
-        list.push(Client {
-            username: username.clone(),
-            stream,
-        });
-    }
+//user ---> proxy ---> budgetchat
+fn handle_write(client: TcpStream, server: &mut TcpStream) {
+    let mut client_reader = BufReader::new(&client);
 
     loop {
-        let mut buffer: String = String::new();
-        match reader.read_line(&mut buffer) {
+        let mut write_buffer = String::new();
+        match client_reader.read_line(&mut write_buffer) {
             Ok(size) if size > 0 => {
-                let mut list = clients.lock().unwrap();
-                for client in list.iter_mut() {
-                    if client.username != username {
-                        print!("{} > [{}] {}", client.username, username, buffer);
-                        let _ = client
-                            .stream
-                            .write_all(format!("[{}] {}", username, buffer).as_bytes());
-                    }
-                }
+                let new_message = check_boguscoin_address(write_buffer);
+                println!("buffer write: {}", new_message);
+                let _ = server.write(new_message.as_bytes());
+                let _ = server.flush();
             }
             Ok(_) => {
-                println!("Utente {} disconnesso", username);
-                let mut list = clients.lock().unwrap();
-
-                let pos = list.iter().position(|x| x.username == username).unwrap();
-                list.remove(pos);
-
-                for client in list.iter_mut() {
-                    let _ = client
-                        .stream
-                        .write_all(format!("* room left by {}\n", username).as_bytes());
-                    let _ = writer.flush();
-                }
-
-                return;
-            }
-            Err(e) => {
-                eprintln!("Read error: {}", e);
+                println!("Write: Buffer vuoto");
+                let _ = client.shutdown(std::net::Shutdown::Both);
+                let _ = server.shutdown(std::net::Shutdown::Both);
                 break;
             }
-        }
-        // FAIL:server did not send 'ProtoBob51' the quit message for 'SlimyFrank231' within 10 seconds
-        match writer.flush() {
-            Ok(_) => {}
             Err(e) => {
-                println!("errore di flush 139 {:?}", e);
+                println!("Write: Errore: {:?}", e);
                 break;
             }
-        }
+        };
     }
 }
 
-fn validate_username(str: &[u8]) -> bool {
-    for c in str[..str.len() - 1].iter() {
-        if !is_uppercase(c) && !is_lowercase(c) && !is_digit(c) {
-            return false;
-        }
+//user <--- proxy <--- budgetchat
+fn handle_read(client: &mut TcpStream, server: TcpStream) {
+    let mut server_reader = BufReader::new(server);
+    loop {
+        let mut read_buffer = String::new();
+        match server_reader.read_line(&mut read_buffer) {
+            Ok(size) if size > 0 => {
+                let new_message = check_boguscoin_address(read_buffer);
+                println!("buffer read: {}", new_message);
+                let _ = client.write(new_message.as_bytes());
+                let _ = client.flush();
+            }
+            Ok(_) => {
+                println!("Read: Buffer vuoto");
+                break;
+            }
+            Err(e) => {
+                println!("Read: Errore: {:?}", e);
+                break;
+            }
+        };
     }
-
-    true
 }
 
-fn is_backslashn(c: &u8) -> bool {
-    *c == 10
+fn handle_connection(client: TcpStream) {
+    let server = TcpStream::connect("chat.protohackers.com:16963").unwrap();
+
+    let client_write_clone = client.try_clone().unwrap();
+    let mut client_read_clone = client.try_clone().unwrap();
+    let mut server_write_clone = server.try_clone().unwrap();
+    let server_read_clone = server.try_clone().unwrap();
+
+    thread::spawn(move || handle_write(client_write_clone, &mut server_write_clone));
+    thread::spawn(move || handle_read(&mut client_read_clone, server_read_clone));
 }
 
-fn is_uppercase(c: &u8) -> bool {
-    *c >= 65 && *c <= 90
+//7[a-zA-Z0-9]{25,35}[\s $]
+fn check_boguscoin_address(buffer: String) -> String {
+    let re = Regex::new(r"7[a-zA-Z0-9]{25,35}[\s $]/s").unwrap();
+    let new_buffer: String = re
+        .replace_all(buffer.as_str(), "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
+        .into_owned();
+
+    new_buffer
 }
 
-fn is_lowercase(c: &u8) -> bool {
-    *c >= 97 && *c <= 122
-}
+//
+// fn check_boguscoin_address(buffer: String) -> String {
+//     let re_s = Regex::new(r"7[a-zA-Z0-9]{25,34}\s").unwrap();
+//     let re_ns = Regex::new(r"7[a-zA-Z0-9]{25,34}$").unwrap();
 
-fn is_digit(c: &u8) -> bool {
-    *c >= 48 && *c <= 57
-}
+//     let new_buffer: String = re_s
+//         .replace_all(buffer.as_str(), "7YWHMfk9JZe0LM0g1ZauHuiSxhI ")
+//         .into_owned();
+
+//     re_ns
+//         .replace_all(new_buffer.as_str(), "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
+//         .into_owned()
+// }
